5b1822440b6b0b15d5eb62135ee4a41b6cd467f3
Caio Vinícius <71348567+SadS4ndWiCh@users.noreply.github.com>
2023-05-27 08:27:50 -0300

    refactor: added game title and price in promotion cards (#1)
    * refactor(slider): add details with gradient background
    
    * refactor(slider): add slider styles
    
    * refactor(slider): add dinamicly the promotions details

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const produtoPromocaoImagem = document.getElementById("img-promocao");
+const produtoPromocaoNome = document.querySelector(".slide--imagem .slide--imagem__detalhes h2");
+const produtoPromocaoPrecoAntigo = document.querySelector(".slide--imagem .slide--imagem__detalhes .slide--imagem__preco del");
+const produtoPromocaoPrecoNovo = document.querySelector(".slide--imagem .slide--imagem__detalhes .slide--imagem__preco strong");
+
 let count = 1;
 document.getElementById("radio1").checked = true;
 
@@ -9,27 +14,47 @@ function radioselect(radioselect)
 {
     if(radioselect == 1)
     {
-        document.getElementById("img-promocao").src = "img/promocao/produto1.jpg";
+        produtoPromocaoImagem.src = "img/promocao/produto1.jpg";
+        produtoPromocaoNome.innerHTML = "Grand Theft Auto";
+        produtoPromocaoPrecoAntigo.innerHTML = "R$ 82,00";
+        produtoPromocaoPrecoNovo.innerHTML = "R$ 41,00";
+
         count = radioselect;
     }
     else if(radioselect == 2) 
     {
-        document.getElementById("img-promocao").src = "img/promocao/produto2.jpg";
+        produtoPromocaoImagem.src = "img/promocao/produto2.jpg";
+        produtoPromocaoNome.innerHTML = "Fifa 2023";
+        produtoPromocaoPrecoAntigo.innerHTML = "R$ 349,00";
+        produtoPromocaoPrecoNovo.innerHTML = "R$ 122,50";
+
         count = radioselect;
     }   
     else if(radioselect == 3)
     {
-        document.getElementById("img-promocao").src = "img/promocao/produto3.jpg";
+        produtoPromocaoNome.src = "img/promocao/produto3.jpg";
+        produtoPromocaoNome.innerHTML = "Hogwarts Legacy";
+        produtoPromocaoPrecoAntigo.innerHTML = "R$ 600,00";
+        produtoPromocaoPrecoNovo.innerHTML = "R$ 230,20";
+
         count = radioselect;
     }
     else if(radioselect == 4)
     {
-        document.getElementById("img-promocao").src = "img/promocao/produto4.jpg";
+        produtoPromocaoNome.src = "img/promocao/produto4.jpg";
+        produtoPromocaoNome.innerHTML = "Outlast";
+        produtoPromocaoPrecoAntigo.innerHTML = "R$ 122,00";
+        produtoPromocaoPrecoNovo.innerHTML = "R$ 50,00";
+
         count = radioselect;
     }
     else if(radioselect == 5)
     {
-        document.getElementById("img-promocao").src = "img/promocao/produto5.jpg";
+        produtoPromocaoNome.src = "img/promocao/produto5.jpg";
+        produtoPromocaoNome.innerHTML = "God Of War";
+        produtoPromocaoPrecoAntigo.innerHTML = "R$ 77,00";
+        produtoPromocaoPrecoNovo.innerHTML = "R$ 42,00";
+
         count = radioselect;
     }
 }
@@ -39,5 +64,6 @@ function nextImage()
     count++;
     if(count>5) count=1;
     document.getElementById("radio"+count).checked = true;
-    document.getElementById("img-promocao").src = "img/promocao/produto"+count+".jpg";
+
+    radioselect(count);
 }
